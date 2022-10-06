f811882094c8665bba7c0298f619e5d684493e40
v203 <vuyisamr203@gmail.com>
2022-10-06 17:36:17 +0200

    revert

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Test from "./pages/Test";
+import './App.css';
+import Confirmation from "./pages/confirmation"
+import SpecialsMenu from './components/SpecialsMenu';
+import Cart from './pages/Cart';
+
+
+
+import Home from './pages/home';
+import Thanks from './pages/thanks';
+import ErrorMsg from './pages/ErrorMsg';
+import Ctx from './Ctx';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { total: 0.00 }
+
+    // this.gtot =  AddRemove()
+
+  }
+  render() {
+    return (
+
+      <BrowserRouter>
+        <Ctx>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/Cart/:total/:order" element={<Cart />} />
+            <Route exact path="/Specials" element={<SpecialsMenu />} />
+
+            <Route exact path='/thanks' element={<Thanks />} />
+            <Route exact path='/test' element={<Test />} />
+            <Route exact path='/ErrorMsg' element={<ErrorMsg />} />
+            <Route exact path='/Ctx' element={<Ctx />} />
+            <Route exact path='/Confirmation' element={<Confirmation />} />
+          </Routes>
+        </Ctx>
+
+      </BrowserRouter>
+
+    )
+  }
 }
 
-
 export default App;
+
+
+
